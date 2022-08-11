cee3bf269b0efd4a9cc441adb1868091b2193dc3
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-11 00:41:57 -0400

    Imported english word check from the class repo.

---
+++ caesar_cipher/is_english_word.py
@@ -0,0 +1,26 @@
+import re
+from caesar_cipher.corpus_loader import word_list, name_list
+
+def count_words(text):
+    candidate_words = text.split()
+    word_count = 0
+
+    for candidate in candidate_words:
+        word = re.sub(r'[^A-Za-z]+','', candidate)
+        if word.lower() in word_list or word in name_list:
+            print("english word", word)
+            word_count += 1
+        else:
+            print('not english word or name', word)
+            pass
+
+    return word_count
+
+def check_confidence(text):
+    words = count_words(text)
+    percentage = int(words / len(text.split()) * 100)
+
+    if percentage > 50:
+        return True
+    else:
+        return False
